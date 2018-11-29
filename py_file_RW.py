8561625ece3fecfa4ebc6d80d37c63187ecd8c44
Chang Ho <changck911@gmail.com>
2018-11-29 12:26:22 +0800

    Python爬蟲練習

--- py_file_RW.py
+++ py_file_RW.py
@@ -13,7 +13,26 @@ path = os.path.abspath('.\\data');
 
 if not os.path.isdir(path):
     os.mkdir(path);
-
+#寫入
+#r - 讀取(檔案需存在)
+#w - 新建檔案寫入(檔案可不存在，若存在則清空)
+#a - 資料附加到舊檔案後面(游標指在EOF)
+#r+ - 讀取舊資料並寫入(檔案需存在且游標指在開頭)
+#w+ - 清空檔案內容，新寫入的東西可在讀出(檔案可不存在，會自行新增)
+#a+ - 資料附加到舊檔案後面(游標指在EOF)，可讀取資料
+#b - 二進位模式
 f = open(path+'\\test.txt','w');
+f.write('test123');
+f.close();
+#讀取
+f = open(path+'\\test.txt','r');
+print(f.read());
+f.close();
+#修改
+f = open(path+'\\test.txt','a');
+#f.seek(位移的bit數,0) - 從文件開頭開始
+#f.seek(位移的bit數,1) - 從目前游標位置開始
+#f.seek(位移的bit數,2) - 從目前文件結尾開始
+f.seek(2);
 f.write('test123');
 f.close();
